8976b6a10f27bcbd6c8647966fdcef7d1dda609a
Victor Martell <vmartell@edgecast.com>
2015-10-05 20:17:34 -0700

    mutagen version and dir convert

--- src/dir2album.py
+++ src/dir2album.py
@@ -8,52 +8,60 @@ import hashlib
 import shutil
 import traceback
 import glob
-from mutagen import id3, File
+from mutagen import id3, File, m4a
 
 def _usage():
-    s_help = "Usage:  m3u2f3 playlist_name target_directory\n"
+    s_help = "Usage:  dir2album source_directory target_directory\n"
     s_help += "Options:\n"
     s_help += "\n%5s,\t%s\t\t\t%s\n\n" % ("-h", "--help", "display this help and exit")
 
     print s_help
 
 
-def ProcessPlayList(ps_PlayList="", ps_TargetPath="./"):
+def ProcessDirectory(ps_SourcePath="", ps_TargetPath="./"):
 
     # Loading Playlist
-    with open(ps_PlayList) as INF:
-        MUSIC_FILES = [LINE.strip() for LINE in INF.readlines() if not LINE.startswith('#')]
+    MUSIC_FILES = os.listdir(ps_SourcePath)
 
     # Let's rock
     for s_MusicFile in MUSIC_FILES:
 
-
-        s_FullFile = os.path.abspath( s_MusicFile )
-        print "Processing: %s" %( s_FullFile)
+        #print s_MusicFile
+        s_FullFile = os.path.abspath( "%s/%s" %(ps_SourcePath,s_MusicFile) )
+        #print "Processing: %s" %( s_FullFile)
         s_Name, s_Ext = os.path.splitext(s_FullFile)
         s_Name = os.path.basename(s_Name)
         s_NewFilename = "%s%s" %(hashlib.md5(s_FullFile).hexdigest(),s_Ext)
-        print "Process Name: %s/%s" %(ps_TargetPath,s_Name)
-        print "New Filename: %s" %(s_NewFilename)
+        #print "Process Name: %s/%s" %(ps_TargetPath,s_Name)
+        #print "New Filename: %s" %(s_NewFilename)
 
         try:
 
-            id3_fullFile = id3.ID3(s_FullFile)
+            if s_Ext == ".m4a":
 
-            for k, v in id3_fullFile.items():
-                print "KEY: %s Value: %s" %(k,v)
+                m4a_fullFile = m4a.M4A(s_FullFile)
+                print m4a_fullFile['\xa9alb'].encode("utf-8", "ignore")
 
-            exit()
 
-            pprint.pprint(id3_fullFile)
-            s_TargetDir = "%s/%s" % (ps_TargetPath,hashlib.md5(id3_fullFile.album).hexdigest())
+            elif s_Ext == ".mp3":
+
+                id3_fullFile = id3.ID3(s_FullFile)
+                print id3_fullFile['TALB']
+
+            else:
+                print "unknown extension"
+                print s_Ext
+                exit()
+
+            #pprint.pprint(id3_fullFile)
+            #s_TargetDir = "%s/%s" % (ps_TargetPath,hashlib.md5(id3_fullFile.album).hexdigest())
 
             # New dir ?
-            if not os.path.exists(s_TargetDir):
-                os.makedirs(s_TargetDir)
+            #if not os.path.exists(s_TargetDir):
+            #    os.makedirs(s_TargetDir)
 
-            shutil.copy2( s_FullFile , "%s/"%(s_TargetDir) )
-            os.rename( "%s/%s%s"%(ps_TargetPath,s_Name,s_Ext),  "%s/%s"%(s_TargetDir,s_NewFilename)  )
+            #shutil.copy2( s_FullFile , "%s/"%(s_TargetDir) )
+            #os.rename( "%s/%s%s"%(ps_TargetPath,s_Name,s_Ext),  "%s/%s"%(s_TargetDir,s_NewFilename)  )
 
         except (shutil.Error, IOError) as e:
 
@@ -90,7 +98,7 @@ if __name__ == "__main__":
             sys.exit(1)
 
     try:
-        s_PlayList = args[0]
+        s_SourcePath = args[0]
     except:
         pass
 
@@ -99,10 +107,10 @@ if __name__ == "__main__":
     except:
         pass
 
-    print "Processing Playlist: %s Target Path: %s" %(s_PlayList, s_TargetPath)
+    print "Processing Directory: %s to Target Path: %s" %(s_SourcePath, s_TargetPath)
 
-    if os.path.exists(s_TargetPath) and os.path.isfile(s_PlayList):
-        ProcessPlayList(s_PlayList, s_TargetPath)
+    if os.path.exists(s_TargetPath) and os.path.exists(s_SourcePath):
+        ProcessDirectory(s_SourcePath, s_TargetPath)
 
     else:
         sys.stderr.write("A Provided Path does not exist..\n")
